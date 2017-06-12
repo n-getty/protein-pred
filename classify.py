6b0029dc3542ed67933aeaa56c1a78259a980781
neilgetty <ngetty@anl.gov>
2017-06-12 12:01:57 -0500

    removed fscore, xgboost does not support

--- classify.py
+++ classify.py
@@ -159,27 +159,27 @@ def main(file="feature_matrix.sm.3.csr_2d.npy", file2="False", file3="False"):
     print log_info
     logging.info(log_info)
     tfer = TfidfTransformer()
-    tfer.fit(features)
-    features = tfer.transform(features)
+    #tfer.fit(features)
+    #features = tfer.transform(features)
     if file2 != "False":
         print "Combining kmer feature matrices"
         features2, _ = load_sparse_csr("data/" + file2)
         features2 = features2[:,:-5]
-        tfer.fit(features2)
-        features2 = tfer.transform(features2)
+        #tfer.fit(features2)
+        #features2 = tfer.transform(features2)
         #normalize(features2, copy=False)
         features = hstack([features, features2])
     if file3 != "False":
         print "Combining kmer feature matrices with 3rd file"
         features3, _ = load_sparse_csr("data/" + file3)
         features3 = features3[:,:-5]
-        tfer.fit(features3)
-        features3 = tfer.transform(features3)
+        #tfer.fit(features3)
+        #features3 = tfer.transform(features3)
         #normalize(features3, copy=False)
         features = hstack([features, features3])
 
-    #tfer.fit(features)
-    #features = tfer.transform(features)
+    tfer.fit(features)
+    features = tfer.transform(features)
 
     print features.shape
     #normalize(features, copy=False,axis=0)
