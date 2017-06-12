659482f8d471d9aa6946e1dbb364fa5f86db7130
neilgetty <ngetty@anl.gov>
2017-06-12 16:26:50 -0500

    re-enabled dimensionality reduction for testing removed logistic regression due to poor results and timing added time logging for dimensionality reduction

--- preprocess.py
+++ preprocess.py
@@ -175,7 +175,7 @@ def get_kmer_counts(data, k):
         kmer counts for all sequences in dataset
     """
     data = zip(range(len(data)), data, [k]*len(data))
-    pool = Pool(processes=12)
+    pool = Pool(processes=300)
 
     res = [None] * len(data)
 
