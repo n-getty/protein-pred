806f9aa4e9ce81b5e79eed09b0258a4a5e00ff7b
neilgetty <ngetty@anl.gov>
2017-06-19 13:47:37 -0500

    memory calculated per model

--- classify.py
+++ classify.py
@@ -70,7 +70,7 @@ def test_train_split(clf, split, m):
     if m == 'Random Forest':
         clf.fit(X_train, y_train)
     else:
-        clf.fit(X_train, y_train, eval_set=(X_test, y_test), early_stopping_rounds=2)
+        clf.fit(X_train, y_train, eval_set=[(X_train, y_train), (X_test, y_test)], early_stopping_rounds=2)
     t5, score = top_5_accuracy(clf.predict_proba(X_test), y_test)
     train_pred = clf.predict(X_train)
     train_score = accuracy_score(y_train, train_pred)
