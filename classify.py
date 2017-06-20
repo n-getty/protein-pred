1d1a528af9fca3a478003b5e627846a07ea666df
neilgetty <ngetty@anl.gov>
2017-06-20 10:04:57 -0500

    reimplemented cross validation

--- classify.py
+++ classify.py
@@ -52,8 +52,7 @@ def cross_validation_accuracy(clf, X, labels, skf, m):
             clf.fit(X_train, y_train,
                     eval_set=[(X_train, y_train), (X_test, y_test)],
                     early_stopping_rounds=2,
-                    verbose=False,
-                    eval_metric='mlogloss')
+                    verbose=False,)
         t5, score = top_5_accuracy(clf.predict_proba(X_test), y_test)
         train_pred = clf.predict(X_train)
         train_score = accuracy_score(y_train, train_pred)
@@ -86,8 +85,7 @@ def test_train_split(clf, split, m):
         clf.fit(X_train, y_train,
                 eval_set=[(X_train, y_train), (X_test, y_test)],
                 early_stopping_rounds=2,
-                verbose=False,
-                eval_metric='mlogloss')
+                verbose=False,)
     t5, score = top_5_accuracy(clf.predict_proba(X_test), y_test)
     train_pred = clf.predict(X_train)
     train_score = accuracy_score(y_train, train_pred)
