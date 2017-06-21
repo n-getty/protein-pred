0f44adb4919fd6066545422472aa0b9d29f4cc1f
neilgetty <ngetty@anl.gov>
2017-06-21 09:21:34 -0500

    testing aa 1mers and 2mers only

--- classify.py
+++ classify.py
@@ -272,16 +272,16 @@ def main(size='sm', file2='0', file3='0', red='0', tfidf='1', prune='0', est='32
            XGBClassifier(n_jobs=-1
                           ,n_estimators=int(est)
                           ,objective="multi:softprob"
-                          ,max_depth=4
-                          ,learning_rate=0.05
+                          ,max_depth=6
+                          ,learning_rate=0.1
                           ,colsample_bytree=1
                           ,subsample=1
                           ,min_child_weight=6
                          ),
 
             LGBMClassifier(nthread=-1
-                           ,num_leaves=15
-                           ,learning_rate=0.5
+                           ,num_leaves=63
+                           ,learning_rate=0.1
                            ,n_estimators=int(est)
                            ,colsample_bytree =1
                            ,subsample=1
