3da5fab31cc734b0a14e42dbe89f5803cc58201a
neilgetty <ngetty@anl.gov>
2017-06-21 13:27:23 -0500

    added probability confusion matrix

--- classify.py
+++ classify.py
@@ -349,8 +349,8 @@ def main(size='sm', file2='0', file3='0', red='0', tfidf='0', prune='0', est='16
         logging.info("Time elapsed for dimensionality reduction is %f" %  elapsed)
 
     #features = features.astype('float32')
-    fimp = np.genfromtxt("results/LightGBM.sorted_features")
-    features = features[fimp[0][:1000]]
+    fimp = np.genfromtxt("results/LightGBM.sorted_features", dtype=[int,float])
+    features = features[:,fimp[0][1000]]
     results = classify_all(labels, features, clfs, folds, model_names, cv)
     #results.sort("Split Val Acc", inplace=True, ascending=False)
     results.to_csv("results/" + size + '.' + file2 + '.' + file3 + '.' + red + '.' + tfidf + '.' + prune + '.' + est, sep="\t")
