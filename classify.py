8e2331f7b5170ae7f45a0628937fbdf78860d928
neilgetty <ngetty@anl.gov>
2017-06-23 12:28:07 -0500

    added probability confusion matrix

--- classify.py
+++ classify.py
@@ -246,9 +246,9 @@ def load_data(size, file2, file3):
         features, labels = load_sparse_csr(path + "feature_matrix.3.csr.npz")
         print "AA 1mers 2mers 3mers"
         # features = features[:,32:]
-        # features = hstack((features[:,32:432], features[:,-22:]), format='csr')
+        features = hstack((features[:,32:432], features[:,-22:]), format='csr')
         # features = hstack((features[:,:432], features[:,-22:]), format='csr')
-        features = features[:,-22:]
+        #features = features[:,-22:]
         #features = features[:,:37]
         if file2:
             print "Adding 5mer count features"
