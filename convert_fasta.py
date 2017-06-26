9a358935e0a49d4f6728c87bd61505ab4aee3b0c
neilgetty <ngetty@anl.gov>
2017-06-26 11:00:54 -0500

    saving dataframe with test/train sets and seqs

--- convert_fasta.py
+++ convert_fasta.py
@@ -11,15 +11,18 @@ data = pd.read_csv(file, names=["protein", "sequence"], usecols=[1, 5], delimite
 data.protein = data.protein.astype(str)
 X_train, X_test, y_train, y_test = train_test_split(data.sequence, data.protein, test_size=0.2, random_state=0, stratify=data.protein)
 
-df = pd.DataFrame({'X_train': X_train, 'X_test': X_test, 'y_train': y_train, 'y_test': y_test})
+train_df = pd.DataFrame({'X_train': X_train, 'y_train': y_train})
 
-train_seqs = '>' + df.y_train + '\n' + df.X_train
-test_seqs = '>' + df.y_test + '\n' + df.X_test
+test_df = pd.DataFrame({'X_test': X_test, 'y_test': y_test})
 
-df.train_fasta = train_seqs
-df.test_seqs = test_seqs
+train_seqs = '>' + train_df.y_train + '\n' + train_df.X_train
+test_seqs = '>' + test_df.y_test + '\n' + test_df.X_test
 
-df.to_csv("data/coreseed.df.csv", index=0)
+train_df.train_fasta = train_seqs
+test_df.test_seqs = test_seqs
+
+train_df.to_csv("data/coreseed.train_df.csv", index=0)
+test_df.to_csv("data/coreseed.test_df.csv", index=0)
 
 with open("data/coreseed.train.fasta", mode='wb') as file:
         for x in train_seqs:
