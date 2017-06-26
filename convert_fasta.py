f6f2b537d08bfbec0af98801df8ea4df3fa8bcff
neilgetty <ngetty@anl.gov>
2017-06-26 10:22:00 -0500

    saving dataframe with test/train sets and seqs

--- convert_fasta.py
+++ convert_fasta.py
@@ -8,9 +8,14 @@ file = "data/ref.100ec.pgf.seqs.filter"
 #data = pd.read_csv(file, names=["protein", "sequence"], usecols=[0, 7], delimiter = '\t')
 data = pd.read_csv(file, names=["protein", "sequence"], usecols=[1, 5], delimiter='\t', header=0)
 X_train, X_test, y_train, y_test = train_test_split(data.sequence, data.protein, test_size=0.2, random_state=0, stratify=data.protein)
+df = pd.DataFrame({'X_train': X_train, 'X_test': X_test, 'y_train': y_train, 'y_test': y_test})
+train_seqs = '>' + df.y_train + '\n' + df.X_train
+test_seqs = '>' + df.y_test + '\n' + df.X_test
 
-train_seqs = '>' + y_train + '\n' + X_train
-test_seqs = '>' + y_test + '\n' + X_test
+df.train_fasta = train_seqs
+df.test_seqs = test_seqs
+
+df.to_csv("data/coreseed.df.csv", index=0)
 
 with open("data/coreseed.train.fasta", mode='wb') as file:
         for x in train_seqs:
