4ddef385e99c115e997bb96b6aa7d0b22fa271f5
neilgetty <ngetty@anl.gov>
2017-07-05 16:08:50 -0500

    convert fasta creates seperate fasta for each class

--- convert_fasta.py
+++ convert_fasta.py
@@ -19,8 +19,8 @@ test_df = pd.DataFrame({'X_test': X_test, 'y_test': y_test})
 train_seqs = '>' + train_df.y_train + '\n' + train_df.X_train
 test_seqs = '>' + test_df.y_test + '\n' + test_df.X_test
 
-train_df.train_fasta = train_seqs
-test_df.test_seqs = test_seqs
+train_df['train_fasta'] = pd.Series(train_seqs)
+test_df['test_fasta'] = pd.Series(test_seqs)
 
 train_df.to_csv("data/coreseed.train_df.csv", index=0)
 test_df.to_csv("data/coreseed.test_df.csv", index=0)
@@ -34,14 +34,14 @@ if not os.path.exists(test_dir):
 if not os.path.exists(train_dir):
     os.makedirs(train_dir)
 
-for x in range(len(train_seqs)):
-    idx = train_df.y_train[x]
-    seq = train_seqs[x]
-    with open(train_dir + idx + ".fasta", mode='wb') as file:
+for index, row in train_df.iterrows():
+    idx = row.y_train
+    seq = row.train_fasta
+    with open(train_dir + idx + ".fasta", mode='ab') as file:
             print>>file, seq
 
-for x in range(len(test_seqs)):
-    idx = test_df.y_test[x]
-    seq = test_seqs[x]
-    with open(test_dir + idx + ".fasta", mode='wb') as file:
+for index, row in train_df.iterrows():
+    idx = row.y_test
+    seq = row.test_fasta
+    with open(test_dir + idx + ".fasta", mode='ab') as file:
             print>>file, seq
