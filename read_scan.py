ae7bdface9da179e8e39c60cc123de35f9bd45df
neilgetty <ngetty@anl.gov>
2017-07-07 09:46:00 -0500

    read_scan builds more usable numpy arrays of y_true and preds from hmmscan results

--- read_scan.py
+++ read_scan.py
@@ -1,7 +1,7 @@
 import numpy as np
+from sklearn.metrics import accuracy_score
 
-
-file = "data/coreseed.test/scans/1000.scan"
+file = "data/coreseed.test/scans/all.scan"
 
 preds = []
 y_true = []
@@ -21,4 +21,12 @@ with open(file, mode='rb') as scan:
                 top = []
                 next = True
 
+first_pred = [x[0] for x in preds]
+acc = accuracy_score(y_true, first_pred)
+c=0
+for x in range(len(y_true)):
+    if y_true[x] in preds[x]:
+        c+=1
+print "Top 5 accuarcy:", float(c)/len(y_true)
+print "Accuracy:", acc
 np.savetxt("data/hmm_preds.csv", np.column_stack((preds,y_true)))
