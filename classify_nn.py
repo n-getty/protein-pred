d4ebeab92f247a03b76fc60e0bf3b2effe1b47f1
neilgetty <ngetty@anl.gov>
2017-07-26 11:36:41 -0500

    confusion matrix now logged with time stamp

--- classify_nn.py
+++ classify_nn.py
@@ -128,7 +128,7 @@ def classify(features, labels, use_batches, file, m, batch_size,
         model = build_attention_model(input_shape, nb_classes)
     elif m == "dense":
         model = densenet.DenseNet(nb_classes,
-                                  (1,features.shape[2],1),
+                                  (features.shape[1],1),
                                   depth,
                                   nb_dense_block,
                                   growth_rate,
@@ -279,7 +279,7 @@ def main():
         pass
     elif args.model == 'dense':
         #features = features.reshape(features.shape[0], features.shape[1], 1)
-        features = features.reshape(features.shape[0], 1, features.shape[1] ,1)
+        features = features.reshape(features.shape[0], features.shape[1],1)
         #features = features.reshape(1, features.shape[0], features.shape[1])
     else:
         features = features.reshape(features.shape[0], features.shape[1], 1, 1)
