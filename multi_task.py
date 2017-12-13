4a17f763dbc5a4562245cce15ea6fe0a2a5a2bd4
ngetty <ngetty@hawk.iit.edu>
2017-12-13 15:28:47 -0600

    copied metrics from go prediction challenge started work on chain classification classify now classifies 1 go term per sequence must finish multi-label/chain work

--- multi_task.py
+++ multi_task.py
@@ -229,7 +229,7 @@ def main():
                         variation=model_variation,
                         classes=classes)
     else:
-        maxlen = 1000
+        maxlen = 256
         (x_train, y_train), (x_test, y_test), classes = load_data_coreseed(maxlen)
         model = Res50NT(input_shape=(maxlen, CHARLEN),
                         dense_layers=dense_layers,
